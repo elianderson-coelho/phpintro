217bbbd2bf46c4e6114aa653af0ab09cd9bd7060
elianderson-coelho <eliandersonc@gmail.com>
2021-10-14 18:05:24 -0300

    add exibição da ficha

--- script.php
+++ script.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Script page</title>
+</head>
+<body>
+    
+</body>
+</html>
+
 <?php
 require_once 'competidor.php';
 
@@ -25,11 +39,11 @@ if ($idade >=6 && $idade <= 12){
     } elseif ($idade > 60) {
         $competidor->setCategoria('Idoso');
     } else {
-        echo "IDADE INVÁLIDA";
-    
+        echo "IDADE INVÁLIDA";    
 };
 
+echo '<div class="container">';
 echo '<h1>FICHA DE INSCRIÇÃO</h1>';
 echo '<p> NOME: ' .$competidor->getNome(); '</p>';
 echo '<p> IDADE: ' .$competidor->getIdade(); '</p>';
-echo '<p> CATEGORIA: ' .$competidor->getCategoria(); '</p>';
+echo '<p> CATEGORIA: ' .$competidor->getCategoria(); '</p> </div></div></div>';
